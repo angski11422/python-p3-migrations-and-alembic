631a488487524e7fcdd9992ba9991cb5062ea7de
Angela Palaszewski <delarosa.ang@gmail.com>
2023-05-07 19:34:12 -0600

    code along

---
+++ lib/migrations/versions/b51e8b799e99_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: b51e8b799e99
+Revises: 8dbaefe58131
+Create Date: 2023-05-07 19:25:07.916413
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b51e8b799e99'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
